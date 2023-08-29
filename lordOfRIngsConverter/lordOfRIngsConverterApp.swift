84ab3f13dacc0405ea942c79c32bfd66db34cb68
DaveM7788 <david.mcdermott98@gmail.com>
2023-08-28 23:09:27 -0500

    Initial Commit

---
+++ lordOfRIngsConverter/lordOfRIngsConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  lordOfRIngsConverterApp.swift
+//  lordOfRIngsConverter
+//
+//  Created by David McDermott on 8/28/23.
+//
+
+import SwiftUI
+
+@main
+struct lordOfRIngsConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
