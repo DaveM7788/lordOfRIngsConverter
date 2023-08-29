245460ec26bfc3e5d534b8616f5e428e9ad4f59e
DaveM7788 <david.mcdermott98@gmail.com>
2023-08-28 23:52:31 -0500

    initial commit
    learn swift

--- lordOfRIngsConverter/ContentView.swift
+++ lordOfRIngsConverter/ContentView.swift
@@ -9,13 +9,73 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack {
+            Image("background")
+                .resizable()
+                .edgesIgnoringSafeArea(.all)
+            
+            VStack {
+                Image("prancingpony")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(height: 200)
+                
+                Text("Currency Exchange")
+                    .font(.largeTitle)
+                    .foregroundColor(.white)
+                
+                
+                HStack {
+                    VStack {
+                        HStack {
+                            Image("silverpiece")
+                                .resizable()
+                                .scaledToFit()
+                                .frame(height: 33)
+                            
+                            Text("Silver Piece")
+                                .foregroundColor(.white)
+                                .font(.headline)
+                            
+                            
+                        }
+                        Text("Text Field")
+                    }
+                    
+                    Image(systemName: "equal")
+                        .font(.largeTitle)
+                        .foregroundColor(.white)
+                    
+                    VStack {
+                        HStack {
+                            Text("Gold Piece")
+                                .foregroundColor(.white)
+                                .font(.headline)
+                            
+                            Image("goldpiece")
+                                .resizable()
+                                .scaledToFit()
+                                .frame(height: 33)
+                        }
+                        Text("Text Field")
+                    }
+                }
+                
+                Spacer()
+                
+                HStack {
+                    Spacer()
+                    
+                    Button {
+                        
+                    } label : {
+                        Image(systemName: "info.circle.fill")
+                    }
+                    .font(.largeTitle)
+                    .foregroundColor(.white)
+                }
+            }
         }
-        .padding()
     }
 }
 
